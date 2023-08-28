ad918852b9132fb1753ea048b3d04d7d15560f2f
Arn van der Pluijm <avanderpluijm@digidentity.com>
2023-08-28 20:56:30 +0200

    use faker to generate seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
@@ -8,54 +9,72 @@ async function seed() {
   await prisma.post.deleteMany();
   await prisma.user.deleteMany();
 
-  // Create a user
-  const kyle = await prisma.user.create({
-    data: { name: "kyle", color: "lime-400" },
-  });
-  const sally = await prisma.user.create({
-    data: { name: "sally", color: "orange-400" },
-  });
-  const bob = await prisma.user.create({
-    data: { name: "bob", color: "blue-400" },
-  });
+  // Create 100 users
+  const users = await Promise.all(
+    Array.from({ length: 100 }).map(() =>
+      prisma.user.create({
+        data: {
+          name: faker.internet.displayName(),
+          color: faker.internet.color(),
+        },
+      })
+    )
+  );
 
-  // Create post
-  const post1 = await prisma.post.create({
-    data: {
-      title: "Building a YouTube clone with Qwik",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat urna vel ante volutpat, ut elementum mi placerat. Phasellus varius nisi a nisl interdum, at ultrices ex tincidunt. Duis nec nunc vel urna ullamcorper eleifend ac id dolor. Phasellus vitae tortor ac metus laoreet rutrum. Aenean condimentum consequat elit, ut placerat massa mattis vitae. Vivamus dictum faucibus massa, eget euismod turpis pretium a. Aliquam rutrum rhoncus mi, eu tincidunt mauris placerat nec. Nunc sagittis libero sed facilisis suscipit. Curabitur nisi lacus, ullamcorper eu maximus quis, malesuada sit amet nisi. Proin dignissim, lacus vitae mattis fermentum, dui dolor feugiat turpis, ut euismod libero purus eget dui.",
-      userId: kyle.id,
-    },
-  });
+  const userIds = users.map((user) => user.id);
+  console.log(`Created ${userIds.length} users`);
 
-  const comment1 = await prisma.comment.create({
-    data: {
-      message:
-        "I think now qwik might be a better option. Because react seems to go into the approach of server rendered html for blog articles. Qwik might become the new reactjs or even better.",
-      userId: kyle.id,
-      postId: post1.id,
-      createdAt: new Date("2021-01-01"),
-    },
-  });
+  const posts = await Promise.all(
+    Array.from({ length: userIds.length / 5 }).map(() =>
+      prisma.post.create({
+        data: {
+          title: faker.lorem.sentence(),
+          description: faker.lorem.paragraph({ min: 1, max: 10 }),
+          userId: userIds[Math.floor(Math.random() * userIds.length)],
+          thumbnail: faker.image.url(),
+          createdAt: faker.date.past(),
+        },
+      })
+    )
+  );
+  const postIds = posts.map((post) => post.id);
+  console.log(`Created ${postIds.length} posts`);
 
-  await prisma.comment.create({
-    data: {
-      parentId: comment1.id,
-      message:
-        "Did you need to do anything different with your panda-css config to get both Qwikify Components and native Qwik to work? Or just add it by doing yarn qwik add pandacss?",
-      userId: sally.id,
-      postId: post1.id,
-    },
-  });
+  // Create top level comments
+  const topLevelComments = await Promise.all(
+    Array.from({ length: postIds.length * 10 }).map(() =>
+      prisma.comment.create({
+        data: {
+          message: faker.lorem.paragraph({ min: 1, max: 10 }),
+          userId: userIds[Math.floor(Math.random() * userIds.length)],
+          postId: postIds[Math.floor(Math.random() * postIds.length)],
+          createdAt: faker.date.past(),
+        },
+      })
+    )
+  );
+  const topLevelCommentIds = topLevelComments.map((comment) => comment.id);
+  console.log(`Created ${topLevelCommentIds.length} top level comments`);
 
-  await prisma.comment.create({
-    data: {
-      message: "I am another root comment",
-      userId: bob.id,
-      postId: post1.id,
-    },
-  });
+  // Create replies
+  const replies = await Promise.all(
+    Array.from({ length: topLevelCommentIds.length * 30 }).map(() =>
+      prisma.comment.create({
+        data: {
+          message: faker.lorem.paragraph({ min: 1, max: 10 }),
+          userId: userIds[Math.floor(Math.random() * userIds.length)],
+          postId: postIds[Math.floor(Math.random() * postIds.length)],
+          createdAt: faker.date.past(),
+          parentId:
+            topLevelCommentIds[
+              Math.floor(Math.random() * topLevelCommentIds.length)
+            ],
+        },
+      })
+    )
+  );
+  const replyIds = replies.map((reply) => reply.id);
+  console.log(`Created ${replyIds.length} replies`);
 }
 
 seed();
